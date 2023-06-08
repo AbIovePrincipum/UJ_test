0ac11d8cea5bad9c0452e2bf163b8cb34cda65ce
AbLovePrincipum <134780247+AbLovePrincipum@users.noreply.github.com>
2023-06-08 18:45:01 +0200

    Update TestCase12.py
    adding the test part and uppdating variables

--- TestCase12.py
+++ TestCase12.py
@@ -4,16 +4,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-zenek = webdriver.Chrome()
-zenek.get("https://todolist.james.am/#/")
+driver = webdriver.Chrome()
+driver.get("https://todolist.james.am/#/")
 
-wait = WebDriverWait(zenek, 20)  # Adjust the timeout value as needed
+wait = WebDriverWait(driver, 20)  # Adjust the timeout value as needed
 
 try:
-    zadanie = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".new-todo")))
+    task = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".new-todo")))
+    task_names = []
+
     for i in range(1, 7):
-        zadanie.click()
-        zadanie.send_keys("zakupy")
-        zadanie.send_keys(Keys.ENTER)
+        task_name = "zakupy"
+
+        task.click()
+        task.send_keys(task_name)
+        task.send_keys(Keys.ENTER)
+
+        task_list = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".todo-list li")))
+        task_names = [task.text for task in task_list]
+
+        if task_names.count(task_name) > 1:
+            print(f"Task '{task_name}' already exists in the to-do list.")
+        else:
+            print(f"Task '{task_name}' added successfully to the to-do list.")
 finally:
-    zenek.quit()
+    driver.quit()
